7cd2d9c403be13cc776e41c9bb3d31f464d177d4
Wilton24 <cbasher24@gmail.com>
2024-04-03 22:51:08 +0800

    configured and added tailwindcss

--- sequelize-mastery/src/components/HelloWorld.vue
+++ sequelize-mastery/src/components/HelloWorld.vue
@@ -6,6 +6,22 @@
 
 <template>
 <h1> Hello World</h1>
+<table>
+
+  <thead>
+    <th>
+      Shaman king
+    </th>
+    <th>Murloc</th>
+    <th>Mirana</th>
+  </thead>
+  
+  <tbody>
+    
+  </tbody>
+</table>
+<h1 class="text-red-700"> Hello, niga  </h1>
+<p class="text-[blue]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo ipsum dolorum eligendi, ad quis veritatis cumque quaerat ex sit nihil molestias rem. Suscipit eveniet enim amet commodi deleniti! Amet, maiores!</p>
 
 </template>
 
