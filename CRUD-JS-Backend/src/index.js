61d0db2ecd83b50698ce41bf271b15a41eb75abf
Mitchel Rondina - Acer <whitebeardv1@gmail.com>
2024-05-27 13:39:13 +0800

    docker config added

--- CRUD-JS-Backend/src/index.js
+++ CRUD-JS-Backend/src/index.js
@@ -39,10 +39,10 @@
     
   })
 
-  // app.get(`/single-hero/${id}`, (req,res)=>{
-  //   const id = req.params.id;
-  //   res.status(200).send('success')
-  // })
+  app.get(`/single-hero/:id`, (req,res)=>{
+    const id = req.params.id;
+    res.status(200).send('success')
+  })
 
   app.listen(PORT, ()=>{
   console.log(`Server running on port ${PORT}`); 
