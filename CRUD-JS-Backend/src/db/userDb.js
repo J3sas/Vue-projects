165409811f0cdd3f2a307f2b278635d547de3906
Wilton24 <cbasher24@gmail.com>
2024-06-10 09:51:24 +0800

    created the user authentication in the backend, using bcrypt hashing.

---
+++ CRUD-JS-Backend/src/db/userDb.js
@@ -0,0 +1,12 @@
+export const users = [
+  {
+    id: 1,
+    username: 'John',
+    password: 'Bern'
+  },
+  {
+    id: 2,
+    username: 'Recy',
+    password: 'Desu'
+  }
+]
--- CRUD-JS-Backend/src/index.js
+++ CRUD-JS-Backend/src/index.js
@@ -2,31 +2,63 @@
   import express from "express";
   import cors from 'cors';
   import routeTesting from "./routes/heroesRoutes.js"
+  import bcrypt, { hash } from "bcrypt";
+  import { users } from "./db/userDb.js";
 
   const app = express();
   dotenv.config();
   const PORT = process.env.PORT || 3000;
 
-
   app.use(express.urlencoded({extended:true}));
   app.use(cors());
 
 
-  app.get('/', (req,res)=>{
-    res.send('WELCOME BOI')
+  app.get('/users', (req,res)=>{
+    res.json({users: users})
   })
 
-  app.post('/login', async (req,res)=>{
+  app.get('/hashSample', async(req,res)=>{
+    const hashSample = await bcrypt.hash('sampleHash',10);
+    res.json({
+      hashSample: hashSample,
+      anotherHash: hashSample
+    }).status(200)
+  })
 
-    const salt = await bcrypt.genSalt();
+  app.post('/registration', async (req,res)=>{
 
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const user = {
-      usename: req.body.username,
-      password: salt + req.body.password
+      id: users.length +1,
+      username: req.body.username,
+      password: hashedPassword
     }
-    res.json(user).status(200);
+    users.push(user);
+    
+    res.json(users).status(201);
 
   })
+
+
+  app.post('/login', async(req, res)=>{
+    const user = users.find(user => user.username === req.body.username);
+    if (user == null){
+      res.status(400).json({message: 'No user found'})
+    }
+
+    try{
+      if (await bcrypt.compare(req.body.password, user.password)){
+        res.send('Logged in :D');
+      } else{
+        res.send('Error! Wrong password or username!')
+      }
+    } catch(err){
+      res.status(500).send();
+    }
+
+
+  })
+
 
   app.use('/heroesRoute', routeTesting)
 
