2d29afa767f6ce9285887dcd4d5f8291147e63d2
Mitchel Rondina - Acer <whitebeardv1@gmail.com>
2024-07-19 23:32:54 +0800

    nuxified the app

---
+++ CRUD-Vue-Frontend/NLTC/pages/homepage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Footer from '~/components/Footer.vue';
+</script>
+
+<template>
+    <div class="p-5 bg-slate-100">
+        <h1>HOME PAGE</h1>
+        <p>This is HOME in the face of the earth</p>
+    </div>
+
+    <Footer/>
+</template>
+
+<style>
+
+</style>
